e6b1e71ce406ef1b87cf891bf046388b9d98554b
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-14 15:12:14 -0500

    feat: add additional configuration to a property wrapper initializer

--- Contents.swift
+++ Contents.swift
@@ -2,9 +2,14 @@ import Cocoa
 
 struct Car {
     @Percentage var fuelLevel: Double = 1.0
+    @Percentage var wiperFluidLevel: Double = 0.5
+    @Percentage(upperBound: 2.0) var stereoVolume: Double = 1.5
 }
 
 var myCar = Car()
 myCar.fuelLevel = 1.2
 
 print("Fuel:", myCar.fuelLevel)
+
+myCar.stereoVolume = 2.5
+print("Volume:", myCar.stereoVolume)
--- Sources/Percentage.swift
+++ Sources/Percentage.swift
@@ -9,17 +9,19 @@ import Foundation
 
 @propertyWrapper public struct Percentage {
     private var storage: Double
+    private var upperBound: Double
     
     public var wrappedValue: Double {
         set {
-            storage = max(min(newValue, 1), 0)
+            storage = max(min(newValue, upperBound), 0)
         }
         get {
             return storage
         }
     }
     
-    public init(wrappedValue: Double) {
-        storage = max(min(wrappedValue, 1), 0)
+    public init(wrappedValue: Double, upperBound: Double = 1) {
+        self.upperBound = upperBound
+        storage = max(min(wrappedValue, self.upperBound), 0)
     }
 }
